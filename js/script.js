3ade79d78fa39f9c0c310658bca029c6ea63a619
Caio Melo Lana Alves <caio.alves@mq.local>
2025-07-28 16:49:33 -0300

    c

--- js/script.js
+++ js/script.js
@@ -1,27 +1,54 @@
-        const ctx = document.getElementById('myDoughnutChart').getContext('2d');
-        const myDoughnutChart = new Chart(ctx, {
-            type: 'doughnut',
-            data: {
-                labels: ['Afternoon', 'Evening', 'Morning'],
-                datasets: [{
-                    data: [1890, 1512, 1323],
-                    label: 'orders',
-                    backgroundColor: [
-                        '#5A6ACF',
-                        '#8593ED',
-                        '#C7CEFF'
-                    ],
-                    borderColor: [
-                        'rgba(255, 99, 132, 1)',
-                        'rgba(54, 162, 235, 1)',
-                        'rgba(255, 206, 86, 1)'
-                    ],
-                    borderWidth: 0
-                }]
+const ctx = document.getElementById('myDoughnutChart').getContext('2d');
+const myDoughnutChart = new Chart(ctx, {
+    type: 'doughnut',
+    data: {
+        labels: ['Afternoon', 'Evening', 'Morning'],
+        datasets: [{
+            data: [1890, 1512, 1323],
+            label: 'orders',
+            backgroundColor: [
+                '#5A6ACF',
+                '#8593ED',
+                '#C7CEFF'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)'
+            ],
+            borderWidth: 0,
+           // hoverOffset: 4
+        }]
+    },
+    options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        cutout: '75%', // Makes it look more doughnut-like
+        plugins: {
+            legend: {
+                position: 'bottom', //posição legenda
+                labels: {
+                    usePointStyle: true, //estilo de ponto p legenda
+                    font:{
+                        size:6
+                    }
+                }
             },
-            options: {
-                responsive: true,
-                maintainAspectRatio: false,
-                cutout: '75%' // Makes it look more doughnut-like
+            tooltip: { 
+                callbacks: {
+                    label: function(context) {
+                        let label = context.label || '';
+                        if (label){
+                            label += ': ';
+                        }
+                        if (context.parsed !== null) {
+                            label += context.parsed + '';
+                        }
+                        return label;
+                    }
+                }
+
             }
-        });
+        }
+    }
+});

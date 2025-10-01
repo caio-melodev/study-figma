2d61e1bc9c4ec52853213dd709ad39e8870fa3a1
caio-melodev <caio16mla@gmail.com>
2025-10-01 15:27:57 -0300

    feat: add initial lineChart component(needs refinement)

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,11 @@
-import { Component, signal, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+import {
+  Component,
+  signal,
+  AfterViewInit,
+  ViewChild,
+  ElementRef,
+  ɵresetJitOptions,
+} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import Chart from 'chart.js/auto';
 
@@ -6,68 +13,83 @@ import Chart from 'chart.js/auto';
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App implements AfterViewInit {
   protected readonly title = signal('figma-frontend-angular');
 
   @ViewChild('doughnutChart') doughnutChart!: ElementRef<HTMLCanvasElement>;
   @ViewChild('barChart') barChart!: ElementRef<HTMLCanvasElement>;
+  @ViewChild('lineChart') lineChart!: ElementRef<HTMLCanvasElement>;
 
   ngAfterViewInit() {
     new Chart(this.doughnutChart.nativeElement, {
       type: 'doughnut',
       data: {
         labels: ['Afternoon', 'Evening', 'Morning'],
-        datasets: [{
-          label: 'Votes',
-          data: [40, 32, 28],
-          backgroundColor: [
-            'rgba(255, 99, 132, 0.7)',
-            'rgba(54, 162, 235, 0.7)',
-            'rgba(255, 206, 86, 0.7)'
-          ],
-          borderColor: [
-            'rgba(255, 99, 132, 1)',
-            'rgba(54, 162, 235, 1)',
-            'rgba(255, 206, 86, 1)'
-          ],
-          borderWidth: 0
-        }]
+        datasets: [
+          {
+            label: 'Votes',
+            data: [40, 32, 28],
+            backgroundColor: [
+              'rgba(255, 99, 132, 0.7)',
+              'rgba(54, 162, 235, 0.7)',
+              'rgba(255, 206, 86, 0.7)',
+            ],
+            borderColor: [
+              'rgba(255, 99, 132, 1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+            ],
+            borderWidth: 0,
+          },
+        ],
       },
       options: {
         responsive: false,
-        cutout: '75%' // Makes it look more doughnut-like
-      }
+        cutout: '75%', // Makes it look more doughnut-like
+      },
     });
 
     new Chart(this.barChart.nativeElement, {
       type: 'bar',
       data: {
         labels: ['Afternoon', 'Evening'],
-        datasets: [{
-          label: 'My First Dataset',
-          data: [65, 59, 80, 81, 56, 55, 40],
-          backgroundColor: [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(201, 203, 207, 0.2)'
-          ],
-          borderColor: [
-            'rgb(255, 99, 132)',
-            'rgb(255, 159, 64)'
-          ],
-          borderWidth: 1
-        }]
+        datasets: [
+          {
+            label: 'My First Dataset',
+            data: [65, 59, 80, 81, 56, 55, 40],
+            backgroundColor: ['rgba(255, 99, 132, 0.2)', 'rgba(201, 203, 207, 0.2)'],
+            borderColor: ['rgb(255, 99, 132)', 'rgb(255, 159, 64)'],
+            borderWidth: 1,
+          },
+        ],
       },
       options: {
         responsive: false,
         scales: {
           y: {
-            beginAtZero: true
-          }
-        }
-      }
+            beginAtZero: true,
+          },
+        },
+      },
+    });
+
+    new Chart(this.lineChart.nativeElement, {
+      type: 'line',
+      data: {
+        labels: ['Jan', 'Feb', 'Mar'],
+        datasets: [
+          { label: 'Dataset1', data: [30, 20, 15], borderColor: 'red' },
+          { label: 'Dataset2', data: [20, 60, 15], borderColor: 'purple' },
+        ],
+      },
+      options: {
+        responsive: false,
+        scales: {
+          y: { min: 10, max: 90 },
+        },
+      },
     });
   }
 }
-
